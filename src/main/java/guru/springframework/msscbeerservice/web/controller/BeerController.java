15d5fd7dc010a0383c44d6af19b545ee20d8fc26
Alessio-Zubiani <alessio.zubiani1996@gmail.com>
2023-09-21 12:37:21 +0200

    Aggiunta JPA entity Beer

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -2,14 +2,17 @@ package guru.springframework.msscbeerservice.web.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
@@ -28,8 +31,11 @@ public class BeerController {
 	@PostMapping
 	public ResponseEntity<Object> saveBeer(@RequestBody BeerDto beerDto) {
 		
-		// TODO implement
-		return new ResponseEntity<Object>(HttpStatus.CREATED);
+		beerDto.setId(UUID.randomUUID());
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("location", "/api/v1/beer/".concat(beerDto.getId().toString()));
+		
+		return new ResponseEntity<Object>(headers, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/{beerId}")
@@ -38,5 +44,9 @@ public class BeerController {
 		// TODO implement
 		return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
 	}
+	
+	@DeleteMapping("/{beerId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteBeer(@PathVariable("beerId") UUID beerId) { }
 
 }
